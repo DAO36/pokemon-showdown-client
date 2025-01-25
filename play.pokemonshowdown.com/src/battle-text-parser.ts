cefa6c1bcd2ff950a55ac0b321d1b84e94259c21
DAO36 <mlpfimytl8st@gmail.com>
2025-01-25 18:31:00 -0500

    uh

--- play.pokemonshowdown.com/src/battle-text-parser.ts
+++ play.pokemonshowdown.com/src/battle-text-parser.ts
@@ -185,16 +185,7 @@ class BattleTextParser {
 
 		case 'cant': {
 			let [, pokemon, effect, move] = args;
-			if (['ability: Damp', 'ability: Dazzling', 'ability: Queenly Majesty', 'ability: Armor Tail'].includes(effect)) {
-				args[0] = '-block';
-				return {args: ['-block', pokemon, effect, move, kwArgs.of], kwArgs: {}};
-			}
-			break;
-		}
-
-		case 'cant': {
-			let [, pokemon, effect, move] = args;
-			if (['ability: Yandere'].includes(effect)) {
+			if (['ability: Damp', 'ability: Dazzling', 'ability: Queenly Majesty', 'ability: Armor Tail', 'ability: Yandere'].includes(effect)) {
 				args[0] = '-block';
 				return {args: ['-block', pokemon, effect, move, kwArgs.of], kwArgs: {}};
 			}
