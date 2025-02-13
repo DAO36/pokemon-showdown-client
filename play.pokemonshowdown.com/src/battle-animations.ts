bf9921bee3287a01c4575252f40052eee399f024
DAO36 <mlpfimytl8st@gmail.com>
2025-02-13 15:09:07 -0500

    huh

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -963,7 +963,7 @@ export class BattleScene implements BattleSceneStub {
 			} else if (this.battle.weatherTimeLeft !== 0) {
 				weatherhtml += ` <small>(${this.battle.weatherTimeLeft} turn${this.battle.weatherTimeLeft === 1 ? '' : 's'})</small>`;
 			}
-			const nullifyWeather = this.battle.abilityActive(['Air Lock', 'Cloud Nine']);
+			const nullifyWeather = this.battle.abilityActive(['Air Lock', 'Cloud Nine', 'Cleaner']);
 			weatherhtml = `${nullifyWeather ? '<s>' : ''}${weatherhtml}${nullifyWeather ? '</s>' : ''}`;
 		}
 
@@ -992,7 +992,7 @@ export class BattleScene implements BattleSceneStub {
 		if (!this.animating) return;
 		let isIntense = false;
 		let weather = this.battle.weather;
-		if (this.battle.abilityActive(['Air Lock', 'Cloud Nine'])) {
+		if (this.battle.abilityActive(['Air Lock', 'Cloud Nine', 'Cleaner'])) {
 			weather = '' as ID;
 		}
 		let terrain = '' as ID;
