08389f8cada4ca47c7d6815e8a4ea074d26f3602
DAO36 <mlpfimytl8st@gmail.com>
2026-05-10 18:02:14 -0400

    D
    ed

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -1330,7 +1330,7 @@ export class BattleScene implements BattleSceneStub {
 			const heart1 = new Sprite(BattleEffects.heart, {
 				display: 'block',
 				x: x + side.leftof(-40),
-				y: y - 10,
+				y: y + 10,
 				z: side.z,
 				opacity: 0.8,
 				scale: 0.4,
@@ -1339,7 +1339,7 @@ export class BattleScene implements BattleSceneStub {
 			const heart2 = new Sprite(BattleEffects.heart, {
 				display: 'block',
 				x: x + side.leftof(-20),
-				y: y - 40,
+				y: y + 40,
 				z: side.z,
 				opacity: 0.8,
 				scale: 0.4,
@@ -1348,7 +1348,7 @@ export class BattleScene implements BattleSceneStub {
 			const heart3 = new Sprite(BattleEffects.heart, {
 				display: 'block',
 				x: x + side.leftof(30),
-				y: y - 20,
+				y: y + 20,
 				z: side.z,
 				opacity: 0.8,
 				scale: 0.4,
@@ -1357,7 +1357,7 @@ export class BattleScene implements BattleSceneStub {
 			const heart4 = new Sprite(BattleEffects.heart, {
 				display: 'block',
 				x: x + side.leftof(10),
-				y: y - 30,
+				y: y + 30,
 				z: side.z,
 				opacity: 0.8,
 				scale: 0.4,
